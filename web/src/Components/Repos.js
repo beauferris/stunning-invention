1a98835619490dd3af992ba5764e29e848f05b94
beauferris <h-beauferris@hotmail.com>
2022-09-27 19:47:47 -0600

    Added buttons to filter between different languages

--- web/src/Components/Repos.js
+++ web/src/Components/Repos.js
@@ -5,24 +5,25 @@ const repos = () => {
   const url = 'http://localhost:4000/repos';
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [repoData, setRepoData] = useState([]);
-
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [filteredData, setFilteredData] = useState([]);
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     const getData = async () => {
       const data = await fetch(url);
       const myData = await data.json();
-      let sortedData = myData;
-      sortedData = myData.sort(function (a, b) {
+      const sortedData = myData.sort(function (a, b) {
         const c = new Date(a.created_at);
         const d = new Date(b.created_at);
         return d - c;
       });
       setRepoData(sortedData);
+      setFilteredData(sortedData);
     };
     getData();
-  }, [repoData]);
+  }, []);
 
-  const repoCards = repoData.map((repo) => {
+  const repoCards = filteredData.map((repo) => {
     return (
       <RepoCard
         key={repo.id}
@@ -35,7 +36,33 @@ const repos = () => {
     );
   });
 
-  return <div className="repos">{repoCards}</div>;
+  const filterData = (event) => {
+    let repoCopy = [...repoData];
+    repoCopy = repoCopy.filter(
+      (repo) => repo.language === event.currentTarget.value
+    );
+    setFilteredData(repoCopy);
+  };
+  const clearFilter = () => {
+    setFilteredData(repoData);
+  };
+
+  const languages = repoData.map((data) => data.language);
+  const languageFilter = [...new Set(languages)].map((language) => {
+    return (
+      <button key={language} value={language} onClick={filterData}>
+        {language}
+      </button>
+    );
+  });
+
+  return (
+    <>
+      <button onClick={clearFilter}>ALL</button>
+      {languageFilter}
+      <div className="repos">{repoCards}</div>
+    </>
+  );
 };
 
 export default repos;
